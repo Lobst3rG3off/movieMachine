22df27a2ec7975e2f094ab9988e661b56fd37dee
Jeffrey Yeung <jeffyyw@gmail.com>
2019-05-22 17:59:15 -0400

    inital ajax testing phase

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const app = {};
 
+app.key = `384043517e4aa58b6327d0789e3e5851`;
 
 
+app.getMovies = (word) => {
+    $.ajax({
+        url: `https://api.themoviedb.org/3/search/movie`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            api_key: app.key,
+            language: `en-US`,
+            query: word,
+            page: 1
+        }
+    }).then(function (results) {
+        console.log(results)
+    })
+}
 
 
-
-app.init() = () => {
-    
+app.init = () => {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        if ($("#search").val() != '') {
+            const searchTerm = $('#search').val();
+            // $('#search').val('');
+            app.getMovies(searchTerm);
+        } else {
+            alert("Please enter a word!");
+        }
+    });   
 }
 
 
